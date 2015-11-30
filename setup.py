e8baa715d1b60024302973674a4ebd9aca7151da
Charles Butler <chuck@dasroot.net>
2015-11-30 09:07:22 -0500

    Initial import

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "charms.docker",
+    version = "0.0.1",
+    author = "Charles Butler",
+    author_email = "charles.butler@ubuntu.com",
+    description = ( "Python wrappers for the docker CLI and configuring the Docker Daemon in Juju Charms" ),
+    license = "GPLv3",
+    keywords = "docker juju charm charms",
+    packages = [],
+    long_description = "",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utiliites",
+        "License :: OSI Approved :: GPLv3 License",
+    ],
+)
