3af9de4e1cd0329673d634ebb455c056aff074ce
Charles Butler <chuck@dasroot.net>
2016-05-12 20:56:58 -0400

    0.1.8 release
    0.1.8 release - includes the private registry login submission by simonklb

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "charms.docker",
-    version = '0.1.6',
+    version = '0.1.8',
     author = "Charles Butler",
     author_email = "charles.butler@ubuntu.com",
     url = "http://github.com/juju-solutions/charms.docker",
