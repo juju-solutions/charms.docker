b3994ca08f1f2d1d12a068f2c445677e3423ab5f
Charles Butler <chuck@dasroot.net>
2017-06-05 12:16:20 -0500

    0.1.17 rev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "charms.docker",
-    version = '0.1.16',
+    version = '0.1.17',
     author = "Charles Butler",
     author_email = "charles.butler@ubuntu.com",
     url = "http://github.com/juju-solutions/charms.docker",
