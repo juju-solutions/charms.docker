e67e5a268f0faeb6667106fa64dacc4007e91351
Cory Johns <johnsca@gmail.com>
2018-09-27 18:44:30 -0400

    Fix long description for PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
 
+with open('README.md') as fp:
+    long_desc = fp.read()
+
 setup(
-    name = "charms.docker",
-    version = '0.1.18',
-    author = "Charles Butler",
-    author_email = "charles.butler@ubuntu.com",
-    url = "http://github.com/juju-solutions/charms.docker",
-    description = ( "Python wrappers for the docker CLI and configuring the Docker Daemon in Juju Charms" ),
-    license = "GPLv3",
-    keywords = "docker juju charm charms",
-    packages = ['charms.docker'],
-    long_description = "",
-    classifiers = [
+    name="charms.docker",
+    version='0.1.19',
+    author="Charles Butler",
+    author_email="charles.butler@ubuntu.com",
+    url="http://github.com/juju-solutions/charms.docker",
+    description=("Python wrappers for the docker CLI and configuring the "
+                 "Docker Daemon in Juju Charms"),
+    long_description_content_type="text/markdown",
+    long_description=long_desc,
+    license="GPLv3",
+    keywords="docker juju charm charms",
+    packages=['charms.docker'],
+    classifiers=[
         "Development Status :: 3 - Alpha",
     ],
 )
